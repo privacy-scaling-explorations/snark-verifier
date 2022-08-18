a11b9c1748dca74713816fddfa64b65ed34cc4c3
Han <tinghan0110@gmail.com>
2022-08-19 01:18:38 +0800

    fix: upgrade adn pin `foundry_evm` version and upgrade `halo2_curve` to `0.2.1` (#4)

--- src/loader/evm/test.rs
+++ src/loader/evm/test.rs
@@ -1,7 +1,7 @@
 use crate::{loader::evm::test::tui::Tui, util::Itertools};
 use foundry_evm::{
     executor::{backend::Backend, fork::MultiFork, ExecutorBuilder},
-    revm::AccountInfo,
+    revm::{AccountInfo, Bytecode},
     utils::h256_to_u256_be,
     Address,
 };
@@ -33,8 +33,10 @@ pub fn execute(code: Vec<u8>, calldata: Vec<u8>) -> (bool, u64, Vec<u64>) {
         .set_debugger(debug)
         .build(Backend::new(MultiFork::new().0, None));
 
-    evm.backend_mut()
-        .insert_account_info(callee, AccountInfo::new(0.into(), 1, code.into()));
+    evm.backend_mut().insert_account_info(
+        callee,
+        AccountInfo::new(0.into(), 1, Bytecode::new_raw(code.into())),
+    );
 
     let result = evm
         .call_raw(caller, callee, calldata.into(), 0.into())
