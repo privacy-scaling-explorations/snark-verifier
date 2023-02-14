c511cf00bb5a201a8d5cff769e47a913723c1e93
han0110 <tinghan0110@gmail.com>
2023-02-14 21:05:46 +0800

    fix: enable `util::hash::poseidon` only when `feature = loader_halo2`

--- snark-verifier/src/util/hash.rs
+++ snark-verifier/src/util/hash.rs
@@ -1,7 +1,9 @@
 //! Hash algorithms.
 
+#[cfg(feature = "loader_halo2")]
 mod poseidon;
 
+#[cfg(feature = "loader_halo2")]
 pub use crate::util::hash::poseidon::Poseidon;
 
 #[cfg(feature = "loader_evm")]
