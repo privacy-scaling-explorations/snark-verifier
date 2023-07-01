27f8b205d64dfe960ffc8bce1891da491dec54c9
soham <22412996+zemse@users.noreply.github.com>
2023-07-01 10:01:25 +0000

    Derive `num_instance` from `instances` in CircuitExt (#44)

--- snark-verifier-sdk/src/lib.rs
+++ snark-verifier-sdk/src/lib.rs
@@ -102,11 +102,13 @@ impl SnarkWitness {
 }
 
 pub trait CircuitExt<F: Field>: Circuit<F> {
-    /// Return the number of instances of the circuit.
+    /// Return the instances of the circuit.
     /// This may depend on extra circuit parameters but NOT on private witnesses.
-    fn num_instance(&self) -> Vec<usize>;
-
     fn instances(&self) -> Vec<Vec<F>>;
+
+    fn num_instance(&self) -> Vec<usize> {
+        self.instances().iter().map(Vec::len).collect()
+    }
 
     fn accumulator_indices() -> Option<Vec<(usize, usize)>> {
         None
